d495f3b91f3057aba909f0eb91902a5a50b75ed6
Pedro Nieto <pnietom@eservicios.indra.es>
2019-02-19 16:26:50 +0100

    Pruebas de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    HelloComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
